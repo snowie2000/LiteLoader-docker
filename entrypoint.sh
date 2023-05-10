c1130a52eeeeaca16b5bd97440804be5caf5efcc
snowie2000 <mactype.feedback@gmail.com>
2023-05-10 10:40:47 +0800

    upgrade bds automatically

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,9 @@ if [ -d "/server_prebuilt" ]; then
     rm -r /server_prebuilt
 fi
 cd /server/bedrock_server
+if [ -f "bedrock_server.exe" ]; then
+    WINEDEBUG=-all wine LLPeEditor.exe
+else
+    echo "File bedrock_server.exe not found in working directory, no further operation is required"
+fi
 wine bedrock_server_mod.exe
